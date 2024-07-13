b6bc1de06bbd9e56bde4986e1d326d19029e02e8
Olha Rypich <olha.rypich@gmail.com>
2024-07-13 17:01:38 +0300

    make build

---
+++ index.61a44b22.js
@@ -0,0 +1,2 @@
+!function e(n,r){var t=document.createElement("ul");for(var a in r){var c=document.createElement("li");c.innerText=a,t.append(c),Object.keys(r[a]).length>0&&e(c,r[a])}n.append(t)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.61a44b22.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const list = document.createElement("ul");
+    for(const key in data){
+        const newLi = document.createElement("li");
+        newLi.innerText = key;
+        list.append(newLi);
+        if (Object.keys(data[key]).length > 0) createTree(newLi, data[key]);
+    }
+    element.append(list);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
